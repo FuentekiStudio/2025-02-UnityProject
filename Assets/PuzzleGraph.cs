038971cd870485a6e3fad1258b377e33e40ba942
Demian <demianlozanoriedel@gmail.com>
2025-12-01 13:06:14 -0300

    [UPDATE] Avoid Nodes Mechanic for PuzzleGraph
    Algo no funciona bien

--- Assets/PuzzleGraph.cs
+++ Assets/PuzzleGraph.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,7 @@ using UnityEngine;
 public class PuzzleGraph : MonoBehaviour
 {
     [SerializeField] List<PuzzleNode> nodes;
+    [SerializeField] List<PuzzleNode> avoidNodes;
 
     private Graph graph;
 
@@ -19,6 +21,9 @@ public class PuzzleGraph : MonoBehaviour
 
     public gateScript bill;
     private bool victory = false;
+
+    //Booleana para verificar conexion con nodos no deseados / avoidNodes
+    private bool badNode = false;
 
     // Start is called before the first frame update
     void Start()
@@ -71,17 +76,50 @@ public class PuzzleGraph : MonoBehaviour
     public bool WinCondition()
     {
         DijkstraResult result = dijkstra.Ejecutar(originNode.id);
+        List<DijkstraResult> avoidNodesResult = new List<DijkstraResult>();
 
-        Debug.Log("Distancias:");
-        for(int i = 0; i < result.Vertices.Count; i++)
+        foreach (PuzzleNode node in avoidNodes)
         {
-            Debug.Log("Nodo " + result.Vertices[i] + ": " + result.Distancia[i]);
+            avoidNodesResult.Add(dijkstra.Ejecutar(node.id));
         }
-        Debug.Log("___________________");
+
+        //Debug.Log("Distancias:");
+        //for(int i = 0; i < result.Vertices.Count; i++)
+        //{
+        //    Debug.Log("Nodo " + result.Vertices[i] + ": " + result.Distancia[i]);
+        //}
+        //Debug.Log("___________________");
 
         List<int> path = dijkstra.ObtenerCamino(originNode.id, destinyNode.id, result);
         int indiceDestino = result.Vertices.IndexOf(destinyNode.id);
         int distance = result.Distancia[indiceDestino];
+
+
+        int c = avoidNodes.Count;
+        foreach (DijkstraResult avoidResult in avoidNodesResult)
+        {
+            int i = 0;
+
+            List<int> avoidPath = dijkstra.ObtenerCamino(originNode.id,avoidNodes[i].id, avoidResult);
+            int indexDestino = avoidResult.Vertices.IndexOf(destinyNode.id);
+            int distanceToDestiny = avoidResult.Distancia[indexDestino];
+
+            if (avoidPath.Count != 0 && distanceToDestiny >= 1)
+            {
+                badNode = true;
+                Debug.Log("Bad Node: " + badNode);
+            }
+            else
+            {
+                badNode = false;
+            }
+
+            if (i+1 <= c)
+            {
+                i++;
+            }
+        }
+        
 
         string pathString = "Path: ";
         foreach (int value in path)
@@ -112,7 +150,7 @@ public class PuzzleGraph : MonoBehaviour
     {
         
         bool win = WinCondition();
-        if (win){
+        if (win && !badNode){
             bill.openedGate = false;
             bill.ChangeSprite();
         }else{
